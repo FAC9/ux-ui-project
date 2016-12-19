1e9ff1af366c062984c116a30aab443333713f25
Peter Rhodes <rhodespeter@hotmail.co.uk>
2016-12-19 16:07:26 +0000

    Delete handlebars files

--- views/handlebars.js
+++
@@ -1,6 +0,0 @@
-var source   = document.getElementById("entry-template").innerHTML
-console.log(source);
-var template = Handlebars.compile(source);
-
-var context = {title: "My New Post", body: "This is my first post!"};
-var html = template(context);
